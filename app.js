1f5d085bab1e4c659eab4cbc82800fffafb4283b
Harish Kumar S <harish@mslabs.in>
2024-08-27 19:24:54 +0530

    refactor: Update default home page message with 3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello, World 2!');
+  res.send('Hello, World 3!');
 });
 
 // Start the server
